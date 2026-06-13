5af271ede6ed7f216418fec626ca441e2ea2f6a1
m4urilio <mrlrex23032006@gmail.com>
2026-06-13 15:05:08 -0300

    revert checkout URL to ggcheckout v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import './index.css'
 
-const CHECKOUT_URL = 'https://pay.hotmart.com/X106313372V?checkoutMode=10'
+const CHECKOUT_URL = 'https://ggcheckout.app/checkout/v5/7F0uplXKgxD3b1Y0y7D7'
 
 const QUIZ_QUESTIONS = [
   {
